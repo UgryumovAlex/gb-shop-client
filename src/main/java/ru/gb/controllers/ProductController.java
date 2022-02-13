451a9cf173dfb2b67222070393a58b2672624642
ugryumov alexey <ugryumov_alex@mail.ru>
2022-02-13 14:57:53 +0300

    initial commit

---
+++ src/main/java/ru/gb/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package ru.gb.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+public class ProductController {
+}
---
+++ src/test/java/ru/gb/controllers/GbShopClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbShopClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
